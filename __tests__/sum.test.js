e12a68a7a6b0192b7a535109c1ed49dd3bfc42b7
Victor Cotrim <cotrim149@gmail.com>
2018-05-28 15:56:47 -0300

    Addong matchers to test null. undefined and false

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -14,3 +14,23 @@ test('object assignment', () => {
   data['two'] = 2;
   expect(data).toEqual({ one: 1, two: 2 });
 });
+
+// using matchers with null, undefined and false
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
+});
