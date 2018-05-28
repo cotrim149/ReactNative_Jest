8e88ecd22005dd52f9936bcc1824b09a524b85aa
Victor Cotrim <cotrim149@gmail.com>
2018-05-28 16:10:13 -0300

    Adding matchers for Array

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -64,3 +64,17 @@ test('there is no I in team', () => {
 test('but there is a "stop" in Christoph', () => {
   expect('Christoph').toMatch(/stop/);
 });
+
+//You can check if an array contains a particular item using toContain:
+
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'beer',
+];
+
+test('the shopping list has beer on it', () => {
+  expect(shoppingList).toContain('beer');
+});
